1104e34f88130d44e651890b779dd118e626b22d
JorisLespagnol <46922441+Bwerpophile@users.noreply.github.com.>
2025-01-20 16:07:23 +0100

    Fixing error in js file & html + creating basics operations

---
+++ index.js
@@ -0,0 +1,20 @@
+const add = function (a, b) {
+    return a + b;
+  };
+
+const subtract = function (a, b) {
+    return a - b;
+  };
+
+const multiply = function (a, b) {
+    return a * b;
+  };
+
+const divide = function(a,b) {
+    return a / b;
+  }
+
+  console.log(divide(4,2));
+  console.log(multiply(4,2));
+  console.log(subtract(4,2));
+  console.log(add(4,2));
