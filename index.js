7851085c10bdf4534cc35c8a8241f4b8923afa9c
JorisLespagnol <46922441+Bwerpophile@users.noreply.github.com.>
2025-01-22 10:27:45 +0100

    Button are listen, digit are rendering on display, now we need to find the logic to make operate work

--- index.js
+++ index.js
@@ -17,8 +17,11 @@ const multiply = function (a, b) {
 const divide = function(a,b) {
     return a / b;
   }
-
   
+  const numbContainer = document.querySelector('.numb-container');
+  const numberPads = [];
+
+  /* CREATE THE DIGITS */
   for (let i = 9; i >= 0; i--) {
     const numbContainer = document.querySelector('.numb-container');
     const numberPad = document.createElement('button');
@@ -28,11 +31,72 @@ const divide = function(a,b) {
     numberPad.classList.add('pad-button');
 
     numbContainer.appendChild(numberPad);
+
+    numberPads.push(numberPad);
   }
+ 
 
   let operate = function (fct, a, b) {
     return fct(a,b)
   }
 
+  const screenText = document.querySelector('.screen-text');
+
+  const addBtn = document.querySelector('.addition');
+
+  addBtn.addEventListener('click', () => {
+    return add;
+  })
+
+  const subtractBtn = document.querySelector('.subtract');
+
+  subtractBtn.addEventListener('click', () => {
+    return subtract;
+  })
+
+
+  const multiplyBtn = document.querySelector('.multiply');
+
+  multiplyBtn.addEventListener('click', () => {
+    return multiply ;
+  })
+
+  const divideBtn = document.querySelector('.divide');
+
+  divideBtn.addEventListener('click', () => {
+    return divide ;
+  })
+
+/* EventListner for digit to be display*/
+  // num1 =  numbb;
+
+  let arrNumb1 = [];
+  let arrNumb2 = [];
+ 
+ // let numbToInt = Number(numbSlice); 
+
+  num1 = numberPads.map(button => {
+    button.addEventListener('click',() => {
+      btnChoose = button.innerText;
+      arrNumb1.push(btnChoose);
+      userEnter = parseInt(arrNumb1.join(""), 10);
+      screenText.innerText = userEnter;
+    });
+  });
+
+  num2 = numberPads.map(button => {
+    button.addEventListener('click',() => {
+      btnChoose = button.innerText;
+      arrNumb2.push(btnChoose);
+      userEnter = parseInt(arrNumb2.join(""), 10);
+      screenText.innerText = userEnter;
+    });
+  });
+
+console.log(numb);
+
   console.log(operate(add, 1,3));
+
+
+
   
